dd15da4a3b44a49fec40ad9610d1af0dfd9d9808
Yeixon Morales Gonzalez <alu0101133324@ull.edu.es>
2021-03-29 21:21:27 +0100

    Ejercicio 2 Volumen

---
+++ src/ejercicio-2/volumen.ts
@@ -0,0 +1,30 @@
+import {isConvertible} from './conversor';
+
+export class Tiempo implements isConvertible<[string, number, string]> {
+
+    constructor(){}
+
+    convert(dato: [string, number, string]): number {
+        let result: number = dato[1];
+        if(dato[0] == 'l') {
+            if (dato[2] == 'm3') {
+                result = parseFloat((dato[1] / 1000).toFixed(3));
+            } else if (dato[2] == 'gal') {
+                result = parseFloat((dato[1] / 3.785).toFixed(3));
+            }
+        } else if (dato[0] == 'm3') {
+            if (dato[2] == 'gal') {
+                result = parseFloat((dato[1] * 264.172).toFixed(3));
+            } else if (dato[2] == 'l') {
+                result = parseFloat((dato[1] * 1000).toFixed(3));
+            }
+        } else if (dato[0] == 'gal') {
+            if (dato[2] == 'l') {
+                result = parseFloat((dato[1] * 3.785).toFixed(3));
+            } else if (dato[2] == 'm3') {
+                result = parseFloat((dato[1] / 264.172).toFixed(3));
+            }
+        }
+        return result;
+    }
+}
