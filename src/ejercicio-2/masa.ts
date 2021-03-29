2ddd4806ef132261089baf919fd1045ee6f255bf
Yeixon Morales Gonzalez <alu0101133324@ull.edu.es>
2021-03-29 21:01:33 +0100

    Ejercicio 2 Masa

---
+++ src/ejercicio-2/masa.ts
@@ -0,0 +1,30 @@
+import {isConvertible} from './conversor';
+
+export class Masa implements isConvertible<[string, number, string]> {
+
+    constructor(){}
+
+    convert(dato: [string, number, string]): number {
+        let result: number = dato[1];
+        if(dato[0] == 'kg') {
+            if (dato[2] == 'lb') {
+                result = parseFloat((dato[1] * 2.20462).toFixed(3));
+            } else if (dato[2] == 'oz') {
+                result = parseFloat((dato[1] * 35.274).toFixed(3));
+            }
+        } else if (dato[0] == 'lb') {
+            if (dato[2] == 'oz') {
+                result = parseFloat((dato[1] * 16).toFixed(3));
+            } else if (dato[2] == 'kg') {
+                result = parseFloat((dato[1] * 0.453592).toFixed(3));
+            }
+        } else if (dato[0] == 'oz') {
+            if (dato[2] == 'kg') {
+                result = parseFloat((dato[1] * 0.0283495).toFixed(3));
+            } else if (dato[2] == 'lb') {
+                result = parseFloat((dato[1] * 0.0625).toFixed(3));
+            }
+        }
+        return result;
+    }
+}
